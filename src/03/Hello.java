ab271e815cc6a7bc00a2e8ef0b3adeff0733caa9
danbihyun <sessak02@naver.com>
2025-09-15 17:01:22 +0900

    update

---
+++ src/03/Hello.java
@@ -0,0 +1,16 @@
+// p.52,  p.60
+
+/* 작성자: 가나다
+작성일: 2025.09.15
+범위 지정해서 하는 주석의 경우 Alt + Shift + A */ 
+
+// Class 선언부
+public class Hello { // Class name(Hello)은 파일 명과 동일해야 함.
+
+    public static void main(String[] args) { // String 의 S는 대문자
+
+        // 메소드 블록
+        System.out.println("Hello,Java"); // System 의 첫 S는 대문자, 문장 끝 세미콜론 필수
+
+    }
+}
---
+++ src/03/RunStatemenExample.java
@@ -0,0 +1,14 @@
+// p.64
+
+public class RunStatemenExample {
+    
+    public static void main(String[] args) {
+
+        int x = 1; // 변수 x 선언 후 1 저장
+        int y = 2; // 변수 y 선언 후 2 저장
+        int result = x + y; // 변수 result 선언 후 x + y 값을 저장
+
+        System.out.println(result); // 변수 result 값을 출력
+        
+    }
+}

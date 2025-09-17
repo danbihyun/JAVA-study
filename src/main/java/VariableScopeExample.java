573b99a41deec9edb95008ae8ee6c2254d5cb2ac
danbihyun <sessak02@naver.com>
2025-09-17 15:14:45 +0900

    update

--- src/main/java/VariableScopeExample.java
+++ src/main/java/VariableScopeExample.java
@@ -9,10 +9,14 @@ public class VariableScopeExample {
             int v2; // if문 안에서 변수 v2 선언
             v2 = v1 - 10; // v2 = 15 - 10 = 5
 
+            // System.out.println(v2);
+
         } // if문 밖으로 나오면서 변수 v2 소멸
 
         int v3 = v1 + 5;
         // v3 = v1 + v2 + 5; 일 때, v2는 소멸되어 존재하지 않는 변수가 되었기 때문에 컴파일 에러
+
+        // System.out.println(v3);
 
     }
 
